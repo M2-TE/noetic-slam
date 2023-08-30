34360a381822b5c47a051d622aa8dbecfb55f0f8
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-08-30 16:02:59 +0200

    formatting rosbag play

--- scripts/rosbag-play.sh
+++ scripts/rosbag-play.sh
@@ -3,5 +3,7 @@ if [ $# -eq 0 ]
 then
     echo "Missing path argument (relative to workspace root)"
 else
-    docker exec -it noeticslam sh -c ". /opt/ros/noetic/setup.sh && rosbag play $1"
+    docker exec -it noeticslam sh -c "\
+    . /opt/ros/noetic/setup.sh &&\
+    rosbag play $1"
 fi
