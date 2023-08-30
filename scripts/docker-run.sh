d4f7dcb0b88423964631f0de10dd619700be1c7b
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-08-30 14:09:25 +0200

    executable shell scripts

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -it --name noeticslam --mount type=bind,source="$(pwd)/..",target=/root/repo noeticslam:latest
+docker run -d -it --name noeticslam --mount type=bind,source="$(dirname "$0")/..",target=/root/repo noeticslam:latest
