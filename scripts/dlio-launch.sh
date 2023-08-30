a18af27c311cc6a21fadf29d156383cad4cdedf3
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-08-30 16:15:17 +0200

    fixed dlio save script

--- scripts/dlio-launch.sh
+++ scripts/dlio-launch.sh
@@ -2,4 +2,5 @@
 docker exec -it noeticslam bash -c "\
 . /opt/ros/noetic/setup.bash && \
 . devel/setup.bash && \
-roslaunch direct_lidar_inertial_odometry dlio.launch rviz:=false pointcloud_topic:=/ouster/points imu_topic:=/ouster/imu"
+roslaunch direct_lidar_inertial_odometry dlio.launch rviz:=false pointcloud_topic:=/robot/lidar imu_topic:=/robot/imu"
+# roslaunch direct_lidar_inertial_odometry dlio.launch rviz:=false pointcloud_topic:=/ouster/points imu_topic:=/ouster/imu"
--- scripts/dlio-save.sh
+++ scripts/dlio-save.sh
@@ -6,5 +6,6 @@ else
     docker exec -it noeticslam bash -c "\
     . /opt/ros/noetic/setup.bash &&\
     . devel/setup.bash &&\
-    rosservice call /robot/dlio_map/save_pcd $1 repo/maps"
+    rosservice call /robot/dlio_map/save_pcd $1 /root/repo/maps/ &&\
+    pcl_pcd2ply /root/repo/maps/dlio_map.pcd /root/repo/maps/dlio_map.ply"
 fi
