8ccd4afd58f857b4803149f91049c84846b79d92
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-08-31 11:57:44 +0200

    run script formatting

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-docker stop noeticslam
-docker rm -f noeticslam
 docker build -t noeticslam:latest $(dirname "$0")/..
 xhost +local:docker
 docker run -dit \
     --name noeticslam \
+    --rm \
     --privileged \
     --mount type=bind,source="$(pwd)/$(dirname "$0")"/..,target=/root/repo \
     --env DISPLAY=$DISPLAY \
     --volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     noeticslam:latest
-docker exec -d noeticslam /bin/bash -c '. /opt/ros/noetic/setup.sh && roscore'
+# start roscore
+docker exec -d noeticslam /bin/sh -c '. /opt/ros/noetic/setup.sh && roscore'
+# enter container with bash
 docker exec -it noeticslam /bin/bash
+docker stop noeticslam
