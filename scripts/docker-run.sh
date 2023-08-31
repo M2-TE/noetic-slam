9469170256204f319c2fbf779f480d84be3a413b
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-08-31 13:16:25 +0200

    simplified run command

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 docker build -t noeticslam:latest $(dirname "$0")/..
 xhost +local:docker
-docker run -dit \
+docker run -it \
     --name noeticslam \
     --rm \
     --privileged \
     --mount type=bind,source="$(pwd)/$(dirname "$0")"/..,target=/root/repo \
     --env DISPLAY=$DISPLAY \
     --volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    noeticslam:latest
-# start roscore
-docker exec -d noeticslam /bin/sh -c '. /opt/ros/noetic/setup.sh && roscore'
-# enter container with bash
-docker exec -it noeticslam /bin/bash
-docker stop noeticslam
+    noeticslam:latest \
+    bash -c 'roscore > /dev/null 2>&1 & disown && bash'
