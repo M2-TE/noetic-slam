507708888eb952adb410a320ff7ef994c76bd007
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-09-27 14:56:11 +0200

    tsdf progress

--- src/tsdf/include/tsdf_map.hpp
+++ src/tsdf/include/tsdf_map.hpp
@@ -29,22 +29,61 @@ public:
 
 private:
     inline void insert_point(Eigen::Vector3f position) {
-        // convert from real coordinate to voxel-local
+        // convert from real coordinate to voxel
         Eigen::Vector3i voxelPos = (position * coordToVoxelRatio).cast<int>();
 
-        // figure out which root octant position the voxel will be in
-        Eigen::Vector3i octantPos = voxelPos / rootDimSize;
+        // figure out which root the voxel will be in
+        Eigen::Vector3i rootParentPos = voxelPos / rootDimSize;
+        // voxel position relative to root (why does eigen not have builtin modulo..)
+        Eigen::Vector3i localPos = voxelPos.unaryExpr([](const int x){ return x % rootDimSize; });
         
-        ROS_INFO_STREAM("Point " << position << " reduced to pos " << voxelPos << " at root pos " << octantPos);
+        // pack position into 64 bits
+        DAG_RootPos rootPos = (DAG_RootPos)rootParentPos.x();
+        rootPos |= (DAG_RootPos)rootParentPos.y() << xBits;
+        rootPos |= (DAG_RootPos)rootParentPos.z() << (xBits + yBits);
+
+        // look up root and set up iteration
+        DAG_Node& currentNode = dagRoots[rootPos];
+
+
+        /// SCRAP THE STUFF BELOW
+        // instead, first check if the localPos is present?
+        // nah idk, that doesnt seem like a good idea..
+        // -> back to the drawing board
+
+        // iterate through DAG levels to reach the correct leaf
+        for (int i = 0; i < dagLevelCount - 1; i++) {
+            DAG_Level& level = dagLevels[i];
+
+            // check which octant the voxel falls into (check each dimension individually)
+            uint8_t maskBitOffset = 0;
+            int32_t curDimThreshhold = rootDimSize / (2 << i);
+            maskBitOffset |= (localPos.x() < curDimThreshhold) << 0;
+            maskBitOffset |= (localPos.y() < curDimThreshhold) << 1;
+            maskBitOffset |= (localPos.z() < curDimThreshhold) << 2;
+
+            // 1 bit for each octant
+            uint8_t maskBit = 1 << maskBitOffset;
+
+            // check if this child is already present in current node
+            if (currentNode.childMask & maskBit) {
+                // go one level deeper
+                // might just be able to negate if condition and leave this out
+            }
+            else {
+                // create child node
+                // check hash of child node to see if it already exists
+            }
+        }
     }
 
 private:
     // compile time constants
     static constexpr float voxelToCoordRatio = 0.01f; // every voxel unit represents this distance
     static constexpr float coordToVoxelRatio = 1.0f / voxelToCoordRatio; // reciprocal
-    static constexpr uint32_t dagLevelCount = 3;
-    static constexpr uint32_t leafDimSize = 1; // leaves are cubic 1x1x1 voxels
-    static constexpr uint32_t rootDimSize = leafDimSize << dagLevelCount; // roots are cubic voxels with dim size depending on the number of DAG levels
+    static constexpr int32_t dagLevelCount = 3; // number of levels including DAG_Root and DAG_Leaves
+    static constexpr int32_t leafDimSize = 1; // leaves are cubic 1x1x1 voxels
+    static constexpr int32_t rootDimSize = leafDimSize << dagLevelCount; // roots are cubic voxels with dim size depending on the number of DAG levels
     static constexpr float rootDimSizeReal = (float)rootDimSize * voxelToCoordRatio;
 
     // some renaming for readability
@@ -65,15 +104,12 @@ private:
         uint16_t refcount; // number of parents
         std::array<DAG_Pointer, 8> childPointers; // could make this sparse?
     };
-    struct DAG_Root {
-        DAG_RootPos pos; // key into ordered map
-    };
     struct DAG_Level {
         phmap::flat_hash_map<std::array<DAG_Pointer, 8>, DAG_Pointer> hashmap;
         std::vector<DAG_Node> nodes;
     };
 
     // storage
-    std::array<DAG_Level, dagLevelCount> dagLevels;
-    phmap::btree_map<DAG_RootPos, DAG_Root> dagRoots;
+    std::array<DAG_Level, dagLevelCount - 2> dagLevels;
+    phmap::btree_map<DAG_RootPos, DAG_Node> dagRoots;
 };
