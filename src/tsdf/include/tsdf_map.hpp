bb13e86600590e9b5b8c7c6ac95a157ead02f5b4
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-10-11 11:46:44 +0200

    migrating child indexing

--- src/tsdf/include/tsdf_map.hpp
+++ src/tsdf/include/tsdf_map.hpp
@@ -13,16 +13,6 @@
 
 class TSDF_Map {
 public:
-    TSDF_Map() {
-        // create empty node at each level since index 0 will be the synonym used for invalid indices
-        dagRootLevel.roots.emplace_back();
-        dagLeafLevel.leaves.emplace_back();
-        for (uint i = 0; i < dagLevelCount - 2; i++) {
-            dagLevels[i].nodes.emplace_back();
-        }
-    }
-
-public:
     void add_pointcloud(nav_msgs::PathConstPtr& pMsgPath, sensor_msgs::PointCloud2ConstPtr& pMsgPointcloud) {
         ROS_INFO("adding pointcloud to tsdf");
 
@@ -44,72 +34,71 @@ public:
     }
 
 private:
-    inline void insert_point(Eigen::Vector3f coordPos) {
-        // convert from real coordinate to voxel
-        Eigen::Vector3i voxelPos = (coordPos * coordToVoxelRatio).cast<int>();
+    inline void insert_point(Eigen::Vector3f realPos) {
+        // convert from 1 to voxel position
+        Eigen::Vector3i voxelPos = (realPos * coordToVoxelRatio).cast<int>();
 
         // figure out which root the voxel will be in, as well as its position local to that root
         Eigen::Vector3i voxelRootPos = voxelPos / rootDimSize;
         Eigen::Vector3i localPos = voxelPos.unaryExpr([](const int x){ return x % rootDimSize; });
+        ROS_INFO("Root Dimension = %d", rootDimSize);
         
-        // pack position into 64 bits
+        // pack root position into 64 bits
         DAG_RootPos rootPos = 0;
         rootPos |= (DAG_RootPos)voxelRootPos.x();
         rootPos |= (DAG_RootPos)voxelRootPos.y() << (xBits);
         rootPos |= (DAG_RootPos)voxelRootPos.z() << (xBits + yBits);
 
-        // check if root is already present, create it if not
+        // check if root is already present, create it and its pointer if not
         auto result = dagRootLevel.hashmap.try_emplace(rootPos);
         if (result.second) {
-            // set up pointer to root and then create it
             result.first->second = dagRootLevel.roots.size();
             dagRootLevel.roots.emplace_back();
         }
         DAG_Node& currentNode = dagRootLevel.roots[result.first->second];
 
-        // TODO: iterate cleanly from root to leaf until hitting an invalid node
-        // should also check the constexpr values since dagLevelCount changed
-        return;
+        // iterate through DAG levels to reach the correct leaf
+        for (uint i = 0; i < dagLevels.size(); i++) {
+            DAG_Level& childLevel = dagLevels[i];
 
-        // // iterate through DAG levels to reach the correct leaf
-        // for (uint i = 0; i < dagLevelCount - 2; i++) {
-        //     DAG_Level& childLevel = dagLevels[i];
+            // check which octant the voxel falls into (check each dimension individually)
+            int32_t nodeHalfDimSize = rootDimSize >> (i + 1);
+            uint8_t childIndex = 0;
+            childIndex |= (localPos.x() < nodeHalfDimSize) << 0;
+            childIndex |= (localPos.y() < nodeHalfDimSize) << 1;
+            childIndex |= (localPos.z() < nodeHalfDimSize) << 2;
+            ROS_INFO("Node Dimension (level %d) = %d", i, nodeHalfDimSize);
 
-        //     // check which octant the voxel falls into (check each dimension individually)
-        //     uint8_t childIndex = 0;
-        //     int32_t curDimThreshhold = rootDimSize / (2 << i);
-        //     childIndex |= (localPos.x() < curDimThreshhold) << 0;
-        //     childIndex |= (localPos.y() < curDimThreshhold) << 1;
-        //     childIndex |= (localPos.z() < curDimThreshhold) << 2;
+            // 1 bit for each octant
+            // max value of childIndex here is 7
+            uint8_t maskBit = 1 << childIndex;
 
-        //     // 1 bit for each octant
-        //     // max value of childIndex here is 7
-        //     uint8_t maskBit = 1 << childIndex;
+            // check if this child is already present in current node
+            if (static_cast<uint8_t>(currentNode.childMask) & maskBit) {
+                ROS_INFO("Found child!");
+                // go one level deeper
+                DAG_Pointer childPointer = currentNode.childPointers[childIndex];
+                currentNode = childLevel.nodes[childPointer];
+            }
+            else {
+                ROS_INFO("Didn't find child.");
+                // we know that the child node doesnt exist, so we have to create it and all its children
+                // and then propagate changes towards the root
 
-        //     // check if this child is already present in current node
-        //     if (static_cast<uint8_t>(currentNode.childMask) & maskBit) {
-        //         // go one level deeper
-        //         DAG_Pointer childPointer = currentNode.childPointers[childIndex];
-        //         currentNode = childLevel.nodes[childPointer];
-        //     }
-        //     else {
-        //         // we know that the child node doesnt exist, so we have to create it and all its children
-        //         // and then propagate changes towards the root
+                //
 
-        //         //
+                // create child node
 
-        //         // create child node
+                // create all children, starting from leaf node
+                for (uint k = 0;false;) {
+                    uint32_t nodePointer = (uint32_t)childLevel.nodes.size();
+                    childLevel.nodes.emplace_back(); // todo: still needs to be added to hashmap
+                }
 
-        //         // create all children, starting from leaf node
-        //         for (uint k = 0;;) {
-        //             uint32_t nodePointer = (uint32_t)childLevel.nodes.size();
-        //             childLevel.nodes.emplace_back(); // todo: still needs to be added to hashmap
-        //         }
-
-        //         // since we know all upcoming levels will not be present, cant just create them all?
-        //         break;
-        //     }
-        // }
+                // since we know all upcoming levels will not be present, cant just create them all?
+                break;
+            }
+        }
 
         // access correct root
     }
