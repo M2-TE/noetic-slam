95ea12755dd12731bb8fa019c6bf20a79af61708
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2023-11-09 10:36:02 +0100

    reenabled non-nvidia renderer

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -24,17 +24,17 @@ docker run -it \
 docker_status=$?
 
 # dont mount gpus when none can be found
-# if [ $docker_status -ne 0 ]; then
-#     echo "GPU passthrough failed, falling back to default"
-#     # with intel integrated gpu
-#     docker run -it \
-#         --name noeticslam \
-#         --rm \
-#         --publish 7008:7008/udp \
-#         --publish 7009:7009/udp \
-#         --mount type=bind,source="$(pwd)/$(dirname "$0")"/..,target=/root/repo \
-#         --device=/dev/dri:/dev/dri \
-#         --env DISPLAY=$DISPLAY \
-#         --volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-#         noeticslam:latest
-# fi
+if [ $docker_status -ne 0 ]; then
+    echo "GPU passthrough failed, falling back to default"
+    # with intel integrated gpu
+    docker run -it \
+        --name noeticslam \
+        --rm \
+        --publish 7008:7008/udp \
+        --publish 7009:7009/udp \
+        --mount type=bind,source="$(pwd)/$(dirname "$0")"/..,target=/root/repo \
+        --device=/dev/dri:/dev/dri \
+        --env DISPLAY=$DISPLAY \
+        --volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        noeticslam:latest
+fi
