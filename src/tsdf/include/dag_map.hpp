b69f9bed3ea06aeff49c507826098700fd81e914
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-02-19 15:24:48 +0100

    morton code creation + sort

--- src/tsdf/include/dag_map.hpp
+++ src/tsdf/include/dag_map.hpp
@@ -5,30 +5,45 @@
 #include <limits>
 #include <cmath>
 #include <bitset>
-#include <random>
 //
 #define BOOST_BIND_GLOBAL_PLACEHOLDERS
 #include <boost/math/ccmath/ccmath.hpp>
 #include <Eigen/Eigen>
 #include <parallel_hashmap/phmap.h>
-#include <libmorton/morton.h> // morton lib A
+#ifdef __INTELLISENSE__
 #define __BMI2__
-#include <morton-nd/mortonND_BMI2.h> // morton lib B
+#endif
+#include <morton-nd/mortonND_BMI2.h>
 //
 #include "constants.hpp"
 #include "dag_structs.hpp"
 
 namespace DAG {
-
-
-static constexpr float distPerVoxel = 0.01f; // real distance for each voxel step
-static constexpr uint32_t nDagLevels = 3; // number of DAG levels including root and leaf nodes
-static constexpr std::array<size_t, nDagLevels> voxelSizes = {
-    2, // root
-    2, // 1
-    2, // leaves
+static constexpr double leafResolution = 0.01; // real distance for each voxel step
+static constexpr uint32_t nDagLevels = 3; // number of DAG levels including root and leaf clusters (excluding leaves)
+static constexpr uint32_t nAllLevels = nDagLevels + 1; // nDagLevels including implicit leaf depth
+static constexpr std::array<uint32_t, nAllLevels> get_sizes() {
+    std::array<uint32_t, nAllLevels> arr = {};
+    uint32_t current = 1;
+    for (size_t i = nDagLevels; i > 0; i--) {
+        arr[i] = current;
+        current *= 2;
+    }
+    arr[0] = current;
+    return arr;
+}
+static constexpr std::array<double, nAllLevels> get_resolutions() {
+    std::array<double, nAllLevels> arr = {};
+    double current = leafResolution;
+    for (size_t i = nDagLevels; i > 0; i--) {
+        arr[i] = current;
+        current *= 2.0;
+    }
+    arr[0] = current;
+    return arr;
 };
-
+static constexpr std::array<uint32_t, nAllLevels> dagSizes = get_sizes();
+static constexpr std::array<double, nAllLevels> dagResolutions = get_resolutions();
 struct Scan {
     Eigen::Vector3f position;
     Eigen::Vector3f rotation;
@@ -47,60 +62,51 @@ struct Map {
     }
 
     void insert_scan(Eigen::Vector3f position, Eigen::Quaternionf rotation, const std::vector<Eigen::Vector3f>& scan) {
-        
-        // get sign bit
-        constexpr int16_t offset = 1 << 15;
-        static_assert(sizeof(int16_t) == 2);
-        static_assert(sizeof(uint16_t) == 2);
-        static_assert(static_cast<int16_t>(0) == 0b0);
-        static_assert(std::numeric_limits<int16_t>::min() - offset == 0b0);
-
-        // std::cout << std::numeric_limits<int16_t>::min() << " " << std::bitset<16>(std::numeric_limits<int16_t>::min()) << std::endl;
-        // std::cout << std::numeric_limits<int16_t>::min() << " " << std::bitset<16>(std::numeric_limits<int16_t>::min() - offset) << std::endl;
-
-        // map min(int),...,max(int) to 0,...,max(uint) (-1, 0, 1 need to be right next to each other)
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int16_t> dis(std::numeric_limits<int16_t>::min(), std::numeric_limits<int16_t>::max());
-        // std::uniform_int_distribution<int16_t> dis(-5, 5);
-        for (int i = 0; i < 100000; i++) {
-            int16_t x = dis(gen) - offset;
-            int16_t y = dis(gen) - offset;
-            int16_t z = dis(gen) - offset;
-
-            // todo: map range to 21 bits? or 20
-
-            // mortonnd::MortonNDBmi_3D_64::Encode()
-            
-            uint_fast64_t code = libmorton::morton3D_64_encode((uint_fast32_t)x, (uint_fast32_t)y, (uint_fast32_t)z);
-            uint_fast32_t xu, yu, zu;
-            libmorton::morton3D_64_decode(code, xu, yu, zu);
-
-            if (x == (int16_t)xu && y == (int16_t)yu && z == (int16_t)zu) {
-                // std::cout << "success: " << x << " " << y << " " << z  << std::endl;
-            }
-            else {
-                std::cout << "         " << x << " " << y << " " << z 
-                    << "\n\tcompared:" << xu << " " << yu << " " << zu << std::endl;
-            }
+        for (uint32_t depth = 0; depth < nDagLevels + 1; depth++) {
+            std::cout << "depth " << depth << ": " << dagSizes[depth] << " voxel(s), " << dagResolutions[depth] << " resolution" << std::endl;
         }
-        return;
-        
-        std::vector<uint_fast64_t> mortonCodes;
+
+        std::vector<std::pair<uint_fast64_t, uint32_t>> mortonCodes;
         mortonCodes.reserve(scan.size());
-        for (auto pCur = scan.cbegin(); pCur != scan.cend(); pCur++) {
+        uint32_t currentLevel = 0;
+        uint32_t index = 0;
+        for (auto pCur = scan.cbegin(); pCur != scan.cend(); pCur++, index++) {
             // calculate voxel position in DAG tree
-            // Eigen::Vector3<int32_t> voxelPos32;
-            // remap min<int32_t>, max<int32_t>
-            // to: 0, max<uint32_t>
-            // Eigen::Vector3<uint32_t> voxelPos16;
-            // 
+            typedef uint32_t uint21_t;
+            Eigen::Matrix<uint21_t, 3, 1> vPos = (*pCur * (1.0 / dagResolutions[currentLevel])).cast<uint21_t>();
+            // std::cout << "\n";
+            // std::cout << (int)vPos.x() << " " << (int)vPos.y() << " " << (int)vPos.z() << std::endl;
+            // std::cout << std::bitset<32>(vPos.x()) << " " << std::bitset<32>(vPos.y()) << " " << std::bitset<32>(vPos.z()) << std::endl;
 
+            // map 32-bit int to 21-bits
+            vPos = vPos.unaryExpr([](const uint21_t i) {
+                constexpr uint21_t signBit32 = 1 << 31;
+                constexpr uint21_t signBit21 = 1 << 20;
+                constexpr uint21_t bitmask = signBit21 - 1;
+                return
+                    (i & bitmask) | // limit to 20 bits (note: mask may not be needed, mortonnd masks too)
+                    ((i & signBit32 ^ signBit32) >> 11); // inverted sign bit for 21 bits total
+            });
+            // std::cout << (int)vPos.x() << " " << (int)vPos.y() << " " << (int)vPos.z() << std::endl;
+            // std::cout << std::bitset<21>(vPos.x()) << " " << std::bitset<21>(vPos.y()) << " " << std::bitset<21>(vPos.z()) << std::endl;
             
-            uint_fast32_t x, y, z; // todo
-            mortonCodes.push_back(libmorton::morton3D_64_encode(x, y, z));
+            // create 63-bit morton code from 3x21-bit fields
+            // z order priority will be: x -> y -> z
+            static_assert(mortonnd::MortonNDBmi_3D_64::FieldBits == 21);
+            mortonCodes.emplace_back(mortonnd::MortonNDBmi_3D_64::Encode<uint_fast32_t>(vPos.x(), vPos.z(), vPos.y()), index);
         }
+        // sort via morton codes to obtain z-order
+        std::sort(mortonCodes.begin(), mortonCodes.end(), [](
+            const std::pair<uint_fast64_t, uint32_t>& a,
+            const std::pair<uint_fast64_t, uint32_t>& b) {
+                return a.first > b.first;
+        });
 
+        // testing z order
+        for (auto pCur = mortonCodes.cbegin(); pCur != mortonCodes.cend(); pCur++) {
+            auto index = pCur->second;
+            // std::cout << scan[index].x() << " " << scan[index].y() << " " << scan[index].z() << std::endl;
+        }
 
 
         // 1. calc morton codes
@@ -108,51 +114,6 @@ struct Map {
         // 3? create binary radix tree from sorted data
         // 4. create DAG sub-tree from data
         // 5. calculate each point's normal via approximated nearest neighbour hyperplane
-    }
-    void insert_scan_old(Eigen::Vector3f position, Eigen::Vector3f rotation, const std::vector<Eigen::Vector3f>& scan) {
-        // normal estimation
-        std::vector<Eigen::Vector3f> normals;
-        normals.reserve(scan.size());
-        for (auto pCur = scan.cbegin(); pCur != scan.cend(); pCur++) {
-            // two closest neighbours
-            std::pair<uint32_t, float> directNeighbourA = { {}, std::numeric_limits<float>::max() };
-            std::pair<uint32_t, float> directNeighbourB = directNeighbourA;
-            
-            // iterate through all point to get close neighbours for current point
-            for (auto pComp = scan.cbegin(); pComp != scan.cend(); pComp++) {
-                // squared distance is sufficient for comparison
-                float distSqr = (*pCur - *pComp).squaredNorm();
-                uint32_t index = pComp - scan.cbegin();
-
-                // update 2 direct neighbours
-                if (distSqr < directNeighbourA.second) directNeighbourA = { index, distSqr };
-                else if (distSqr < directNeighbourB.second) directNeighbourB = { index, distSqr };
-            }
-            // std::cout << influencers.size() << std::endl;
-            
-            // calc normal using two neighbours
-            auto vecA = scan[directNeighbourA.first] - *pCur;
-            auto vecB = scan[directNeighbourB.first] - *pCur;
-            auto normal = vecA.cross(vecB).normalized();
-
-            // invert normal if it points in the wrong direction
-            if (normal.dot(*pCur - position) >= 0.0f) normal = -normal;
-            normals.push_back(normal);
-        }
-
-        // todo:
-        // create temporary tree by inserting these scanned points
-        //// for every point:
-        //// 1. create hyperplane
-        //// 2. get leaf cluster pos (and 8 surrounding leaf cluster positions)
-        //// 3. get each leaf cluster from hash table
-        //// 4. calculate real pos of each leaf in leaf cluster
-        //// 5. insert signed distance into leaves
-        static constexpr size_t dim = voxelSizes.back();
-        struct LeafPosition { uint32_t x, y, z; };
-        struct LeafCluster { std::array<float, dim*dim*dim> leaves; };
-        phmap::flat_hash_map<LeafPosition, LeafCluster> tempMap;
-        
     }
 
 private:
