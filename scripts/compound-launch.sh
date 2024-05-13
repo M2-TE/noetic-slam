d77906b9ae90f5fb1212053684121daac10583dc
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-05-13 10:52:47 +0200

    enforce compound

--- scripts/compound-launch.sh
+++ scripts/compound-launch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 if [ $ROS_DISTRO = "noetic" ]; then
+
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    source /opt/ros/noetic/setup.bash 
+    source ${SCRIPT_DIR}/../devel/setup.bash
     # ensure roscore is up and running
     rostopic list &> /dev/null
     roscore_status=$?
@@ -11,32 +15,31 @@ if [ $ROS_DISTRO = "noetic" ]; then
 
     # let user override the mode
     if [ -z "$1" ]; then
-        echo "error: select a mode: replay, record, stream"
+        echo "usage: scripts/compound-launch.sh [replay, record, stream] [bags/*.bag]"
         exit -1
     fi
-    selected_mode=$1
 
     # handle selected mode
     if [ $selected_mode = "replay" ]; then
+        if [ -z "$2" ]; then
+            echo "usage: scripts/compound-launch.sh replay [bags/*.bag]"
+            exit -1
+        fi
         scripts/dlio-launch.sh &
-        DLIO_PID=$!
-        scripts/rosbag-replay.sh
-        # scripts/ouster-replay.sh
-
-        # give dlio some time to fully process the trauma
-        # sleep 2 && scripts/dlio-save.sh
-        # TODO: kill dlio here
+        scripts/rosbag-replay.sh $2
 
     elif [ $selected_mode = "record" ]; then
-        rosbag record -O bags/${FILENAME}_generic.bag ${PCL_TOPIC} ${IMU_TOPIC} &
-        scripts/ouster-record.sh
+        if [ -z "$2" ]; then
+            echo "usage: scripts/compound-launch.sh record [bags/*.bag]"
+            exit -1
+        fi
+        # rosbag record -O bags/${FILENAME}_generic.bag ${PCL_TOPIC} ${IMU_TOPIC} &
+        scripts/ouster-record.sh $2
 
     elif [ $selected_mode = "stream" ]; then
         # stream the data and sic dlio on it
         scripts/dlio-launch.sh &
-        DLIO_PID=$!
         scripts/ouster-stream.sh
-        # TODO: kill dlio here
     else
         echo "Valid modes are replay, record or stream"
     fi
