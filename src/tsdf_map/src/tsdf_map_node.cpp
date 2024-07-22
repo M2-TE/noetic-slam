7265648a4f00547ca3e2a906898286ce24741140
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-07-22 21:58:43 +0200

    sync

--- src/tsdf_map/src/tsdf_map_node.cpp
+++ src/tsdf_map/src/tsdf_map_node.cpp
@@ -31,7 +31,7 @@ public:
         subPcl = nh.subscribe("/robot/dlio/odom_node/pointcloud/keyframe", queueSize, &TSDFMap::callback_pcl_deskewed, this);
         // subPcl = nh.subscribe("/robot/dlio/odom_node/pointcloud/deskewed", queueSize, &TSDFMap::callback_pcl_deskewed, this);
         
-        if (false) {
+        if (true) {
             // generate random point data
             std::vector<Eigen::Vector3f> points(100'000);
             std::random_device rd;
@@ -67,7 +67,7 @@ public:
             dag.reconstruct();
             exit(0);
         }
-        else if (true) {
+        else if (false) {
             
             std::ifstream file;
             file.open("debugpoints.bin", std::ofstream::binary);
