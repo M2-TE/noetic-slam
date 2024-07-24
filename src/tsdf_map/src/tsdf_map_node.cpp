51b5e590aebe99da730c3291bbd9febc4d20d765
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-07-24 10:52:12 +0200

    updated clangd info

--- src/tsdf_map/src/tsdf_map_node.cpp
+++ src/tsdf_map/src/tsdf_map_node.cpp
@@ -18,12 +18,33 @@
 #include <pcl/io/ply_io.h>
 //
 #include <Eigen/Eigen>
-#include "dlio_stuff.hpp"
 #include "dag/dag.hpp"
 
 // todo: move headers to the places that need them
 // todo: separate source for vulkan stuff as well
 // todo: use fmt for logging
+
+struct Point {
+    Point(): data{0.f, 0.f, 0.f, 1.f} {}
+
+    PCL_ADD_POINT4D;
+    float intensity; // intensity
+    union {
+        std::uint32_t t; // time since beginning of scan in nanoseconds
+        float time; // time since beginning of scan in seconds
+        double timestamp; // absolute timestamp in seconds
+    };
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} 
+EIGEN_ALIGN16;
+POINT_CLOUD_REGISTER_POINT_STRUCT(Point,
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (float, intensity, intensity)
+    (std::uint32_t, t, t)
+    (float, time, time)
+    (double, timestamp, timestamp))
 
 class TSDFMap {
 public:
