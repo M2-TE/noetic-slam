b6a025a4dc9fab4b910c525071e2d006020f3b7b
annikaul <annika-kristin.kaul1@informatik.hs-fulda.de>
2024-12-04 16:01:07 +0100

    Save lidar data to path

--- src/lidardatasaver/src/lidardatasaver.cpp
+++ src/lidardatasaver/src/lidardatasaver.cpp
@@ -5,6 +5,10 @@
 #include <pcl/point_types.h>
 #include <yaml-cpp/yaml.h>
 #include <fstream>
+#include <filesystem>
+#include <sstream>
+
+namespace fs = std::filesystem;
 
 class LidarDatasaver {
 public:
@@ -13,16 +17,21 @@ public:
         std::cout << "Lidar datasaver node was started." << std::endl;
         std::cout << "********************************************" << std::endl;
         
+        setLidarTargetDir();
+
         sub_pcl = nh.subscribe("/ouster/points", 10, &LidarDatasaver::callbackPointCloud, this);
     }
 
     // Read point cloud data and save it to seperate files
     void callbackPointCloud(const sensor_msgs::PointCloud2ConstPtr& msg) {
+        fs::create_directories(getNextDir());
+
         pcl::PointCloud<pcl::PointXYZI> cloud;
         pcl::fromROSMsg(*msg, cloud);
 
+
         // std::ofstream dataFileAmplitude("amplitude.data", std::ios::binary);
-        std::ofstream dataFileIntensities("intensities.data", std::ios::binary);
+        std::ofstream dataFileIntensities(getNextDir() + "intensities.data", std::ios::binary);
         
         for (const auto& point : cloud.points) {
             float intensities = point.intensity;
@@ -35,6 +44,8 @@ public:
         createYAML("intensities.yaml", "intensities", "channel", "float", "[59463419, 1]");
         ROS_INFO("Saved intensities data.");
 
+        nextDirNr++;
+
         // TODO: weiter dateien erstellen -> welche Werte werden benötigt?
         // TODO: meta.yaml erstellen
 
@@ -43,6 +54,9 @@ public:
 
 private:
     ros::Subscriber sub_pcl;
+    std::string lidarTargetDir;
+
+    int nextDirNr = 0;
 
     // TODO: Ordner anpassen
     // create YAML file
@@ -54,7 +68,7 @@ private:
         yamlNode["shape"] = YAML::Load(shape);
         yamlNode["name"] = name;
 
-        std::ofstream outFile(filename);
+        std::ofstream outFile(getNextDir() + filename);
         if (outFile.is_open()) {
             outFile << yamlNode;
             outFile.close();
@@ -62,7 +76,34 @@ private:
             ROS_ERROR_STREAM("Failed to create " << filename);
         }
     }
+
+    void setLidarTargetDir() {
+        std::string baseTargetDir = fs::absolute(fs::path(__FILE__).parent_path().parent_path().parent_path().parent_path()).string() + "/sampledata/raw/";
+        std::cout << "*******Base dir: " << baseTargetDir << "*********" << std::endl;
+       
+        int maxDir = 0;
+        for (const auto& entry : fs::directory_iterator(baseTargetDir)) {
+            if (entry.is_directory() && entry.path().filename() != "meta.yaml") {
+                int dirNumber = std::stoi(entry.path().filename().string());
+                maxDir = std::max(maxDir, dirNumber);
+            }
+        }
+
+
+        std::ostringstream lidarTargetDirName;
+        lidarTargetDirName << baseTargetDir << std::setw(8) << std::setfill('0') << maxDir << "/lidar_00000000/" ;
+        lidarTargetDir = lidarTargetDirName.str();
+        std::cout << "*******Lidar dir: " << lidarTargetDir << "*********" << std::endl;
+       
+    }
+
+    std::string getNextDir() {
+        std::ostringstream nextDirName;
+        nextDirName << lidarTargetDir << std::setw(8) << std::setfill('0') << nextDirNr << "/" ;
+        return nextDirName.str();
+    }
 };
+
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "lidardatasaver");
